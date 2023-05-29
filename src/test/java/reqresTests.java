6aaa9fcbad43e3a1b2e367a3f7d2b0e7121aa536
Kate Moiseenko <katfedoseeva@gmail.com>
2023-05-29 23:43:51 +0300

    api tests has been implemented

---
+++ src/test/java/reqresTests.java
@@ -0,0 +1,91 @@
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.is;
+
+public class reqresTests {
+
+    @BeforeAll
+    static void setUp() {
+        RestAssured.baseURI = "https://reqres.in/";
+        RestAssured.basePath = "/api";
+    }
+
+    @Test
+    void getUserByIdResponseBodyValuesTest() {
+        given()
+                .log().uri()
+                .when()
+                .get("/users/2")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("data.id", is(2))
+                .body("data.first_name", is("Janet"))
+                .body("data.last_name", is("Weaver"));
+    }
+
+    @Test
+    void getUserByNotExistedIdStatusCodeTest() {
+        given()
+                .log().uri()
+                .when()
+                .get("/users/23")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(404);
+    }
+
+    @Test
+    void getUsersListSecondPageResponseBodyIdsTest(){
+        given()
+                .log().uri()
+                .when()
+                .get("/users?page=2")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("data.id", hasItems(7, 8, 9, 10, 11, 12));
+    }
+
+    @Test
+    void postUserResponseBodyValuesTest() {
+        String requestBody = "{\"name\": \"morpheus\",\n" +
+                "\"job\": \"leader\"}";
+
+        given()
+                .log().uri()
+                .log().body()
+                .contentType(JSON)
+                .body(requestBody)
+                .when()
+                .post("/users")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(201)
+                .body("name", is("morpheus"))
+                .body("job", is("leader"));
+    }
+
+    @Test
+    void getResourceByIdResponseJsonSchemaTest() {
+        given()
+                .log().uri()
+                .when()
+                .get("/unknown/2")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body(matchesJsonSchemaInClasspath("schemes/single-resource-byid-response-scheme.json"));
+    }
+}
